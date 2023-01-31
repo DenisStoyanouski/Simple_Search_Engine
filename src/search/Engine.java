829372abef4ebef26debd4661df1fb9421e801d4
Denis Stoyanouski <vandenst@tut.by>
2023-01-31 22:48:58 +0300

    add methods printMenu, printAllPeople class Engine

--- src/search/Engine.java
+++ src/search/Engine.java
@@ -34,24 +34,52 @@ class Engine {
             source.add(person);
             numberOfPeople--;
         }
-        enterQuery();
+        System.out.println();
+        printMenu();
+    }
+
+    private static void printMenu() {
+        String menuItem;
+        while(true) {
+            System.out.println(
+                    "=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
+            menuItem = input().trim();
+            switch (menuItem) {
+                case "1" : enterQuery();
+                    break;
+                case "2" : printAllPeople();
+                    break;
+                case "0" :
+                    System.out.println("Bye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.");
+                    break;
+            }
+        }
     }
 
     private static void enterQuery() {
-        int numberOfSearch = 0;
+        int numberOfSearch = 1;
         String data;
-        while (numberOfSearch == 0) {
+        /*while (numberOfSearch == 0) {
             System.out.println("Enter the number of search queries:");
             try {
                 numberOfSearch = Integer.parseInt(input());
             } catch (NumberFormatException e) {
                 System.out.println("Try again");
             }
-        }
+        }*/
+        System.out.println();
 
         while (numberOfSearch != 0) {
-            System.out.println("Enter data to search people:");
+            System.out.println("Enter a name or email to search all suitable people.");
             data = input();
+            System.out.println();
             getSearch(data);
             numberOfSearch--;
         }
@@ -61,8 +89,9 @@ class Engine {
         ArrayList<String> resultOfSearch = new ArrayList<>();
         for (String[] person : source) {
             for (String per : person) {
-                if (data.equalsIgnoreCase(per)) {
+                if (per.toLowerCase().contains(data.toLowerCase())) {
                     resultOfSearch.add(Arrays.toString(person).replaceAll("[\\[\\],]", ""));
+                    break;
                 }
             }
         }
@@ -72,6 +101,12 @@ class Engine {
             System.out.println("Found people:");
             resultOfSearch.forEach(System.out::println);
         }
+        System.out.println();
+    }
+
+    private static void printAllPeople() {
+        System.out.println("=== List of people ===");
+        source.stream().forEach(x-> System.out.println(Arrays.toString(x).replaceAll("[\\[\\],]", "")));
     }
 
     private static String input(){
