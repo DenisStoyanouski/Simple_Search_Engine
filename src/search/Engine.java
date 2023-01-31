a9d7f29bd6ec963c14854f05d0c93d7bc5b7bdb3
Denis Stoyanouski <vandenst@tut.by>
2023-01-31 21:07:42 +0300

    add class Engine

---
+++ src/search/Engine.java
@@ -0,0 +1,12 @@
+package search;
+
+import java.util.Scanner;
+
+class Engine {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private static String input(){
+        return scanner.nextLine();
+    }
+
+}
